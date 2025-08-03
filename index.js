a9e4236e8f9a5006a4996ca3152d5bce7ab484a2
apni-coding <apnicoding72@gmail.com>
2025-08-03 18:26:11 +0530

    react fragment

--- index.js
+++ index.js
@@ -7,20 +7,15 @@ heading.className = "header";
 
 // document.getElementById("root").append(heading);
 
-
 /** REACT  with JS*/
 // const reactHeading = React.createElement("h2", {className : "head", id : "heading", children: "Hello React!"});
 
 /* REACT with JSX  */
-const jsxHeading = (<h2 className="head">Hello JSX!</h2>);
+const jsxHeading = (
+  <>
+    <h2 className="head">Hello JSX!</h2>
+    <p>This is created using JSX</p>
+  </>
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
-
-
-// console.log("React Element", reactHeading)
-
-/*
-1. type of element (h1, h2, div, span)
-2. object -> class, id, attributes //null
-3. text inside the element
-*/

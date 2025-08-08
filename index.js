a065c472f67fcd6fe5be6632082a930ff6af3345
apni-coding <apnicoding72@gmail.com>
2025-08-08 16:00:50 +0530

    expression in jsx

--- index.js
+++ index.js
@@ -4,13 +4,33 @@ function App(){
   var name = "vivek";
   var age = 20
   let demo = null;
+  let undef;
+  const bool = true
 
+  function sum(){
+    let a = 10+5;
+    return a;
+  }
 
+  function a (){
+    return "abc";
+    let c = 20;
+    
+  }
+
+  const heading = <h2>JSX Expression</h2>
   return (
     <>
-    <h1>Hello {name}!</h1>
-    <p>Your age is {age}</p>
-    <h1>{demo} is here</h1>
+    <h1>JavaScript inside JSX</h1>
+    {
+      heading
+    }
+    <p>String: {name}</p>
+    <p>Number: {age}</p>
+    <p>Null Value: {demo}</p>
+    <p>Undefined Value: {undef}</p>
+    <p>Boolean Value: {bool}</p>
+    <p>Calling a function: {sum()}</p>
     </>
   )
 }

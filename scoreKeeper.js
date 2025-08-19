eeaaddf7f2c59133735d0b19cedcbfce9f03e07d
apni-coding <apnicoding72@gmail.com>
2025-08-19 08:28:17 +0530

    more on event handling

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -2,8 +2,8 @@ let score = 0;
 let wicket = 0;
 
 function App() {
-  function scoreOne() {
-    score += 1;
+  function addScore(num) {
+    score += num;
     reactNode.render(<App />);
   }
 
@@ -14,12 +14,12 @@ function App() {
         SCORE: {score}/{wicket}
       </h2>
       <div>
-        <button onClick={scoreOne}>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
+        <button onClick={() => addScore(1)}>1</button>
+        <button onClick={() => addScore(2)}>2</button>
+        <button onClick={() => addScore(3)}>3</button>
+        <button onClick={() => addScore(4)}>4</button>
+        <button onClick={() => addScore(5)}>5</button>
+        <button onClick={() => addScore(6)}>6</button>
       </div>
     </>
   );

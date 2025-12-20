bad0ae9179e29309c22ae7fce98a723638478530
apni-coding <apnicoding72@gmail.com>
2025-12-20 21:08:15 +0530

    dynamic styles in styled components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,64 +1,69 @@
-import React, { Component } from "react";
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Nav = styled.div`
-background:#4267b2;
-height:70px;
+  height: 70px;
+  background: linear-gradient(170deg, #1bc059, #0d47a1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
 `;
 
 const Title = styled.div`
-font-size:30;
-color:#fff;
-margin-left:20
-`
+  font-size: 30px;
+  color: #fff;
+  font-weight: 600;
+  font-family: "Times New Roman", Times, serif;
+  text-transform: uppercase;
+  margin-left: 20px;
+  &:hover{
+    color: #0f0;
+    cursor: pointer;
+  }
+`;
 
-export default class Navbar extends Component {
+const CartImg = styled.img`
+  height: 48px;
+  margin-right: 20px;
+`;
+
+const CartIconContainer = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
+
+const CartCount = styled.div`
+  background: ${(props)=> props.color};
+  border-radius: 50%;
+  padding: 4px 8px;
+  position: absolute;
+  right: 10px;
+  top: -5px;
+  font-size: 12px;
+  visibility: ${(props)=> props.show ? "visible" : "hidden"};
+`;
+
+class Navbar extends React.Component {
   render() {
     return (
       <>
         <Nav>
-          <Title>Movie-App</Title>
-          <div style={styles.cartContainer}>
-            <img alt="cart icon" src="https://cdn-icons-png.flaticon.com/128/891/891462.png" style={styles.cartIcon}/>
-            <span style={styles.cartCount}>0</span>
-          </div>
+          <Title>MOVIE APP</Title>
+
+          <CartIconContainer>
+            <CartImg
+              alt="Cart-Icon"
+              src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
+            />
+            <CartCount color= "yellow" show={true}>
+              5
+            </CartCount>
+          </CartIconContainer>
         </Nav>
       </>
     );
   }
 }
 
-const styles = {
-  nav: {
-    height: "70px",
-    backgroundColor: "#4267b2",
-    display: "flex",
-    justifyContent: "space-between",
-    position:"relative",
-    padding:"10px"
-  },
-  title:{
-    fontSize:30,
-    color:"#fff",
-    fontFamily:'"Montserrat", sans-serif',
-    textTransform:"uppercase",
-    marginLeft:20
-  },
-  cartContainer:{
-    position:"relative",
-    cursor:"pointer"
-  },
-  cartIcon:{
-    height:48,
-    marginRight:20
-  },
-  cartCount:{
-    background:"orange",
-    borderRadius:"50%",
-    padding:"4px 8px",
-    position:"absolute",
-    right:10,
-    top:-5,
-    fontSize:12
-  }
-};
+export default Navbar;

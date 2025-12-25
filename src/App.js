7b904b63b092189036cc05138ac31ad956053c8d
apni-coding <apnicoding72@gmail.com>
2025-12-25 20:15:37 +0530

    state lifting up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
+import { Component } from "react";
 import MovieList from "./MovieList";
 import Navbar from "./Navbar";
+import { movies } from "./movieData";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <MovieList />
-    </>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies,
+      cartCount: 0,
+    };
+  }
+
+  handleIncStar = (movie) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(movie);
+    if (movies[mid].stars >= 5) {
+      return;
+    }
+    movies[mid].stars += 0.5;
+    this.setState({
+      movies,
+    });
+  };
+
+  handleDecStar = (movie) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(movie);
+    if (movies[mid].stars <= 0) {
+      return;
+    }
+    movies[mid].stars -= 0.5;
+    this.setState({
+      movies,
+    });
+  };
+
+  handleToggleCart = (movie) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(movie);
+    movies[mid].isIncart = !movies[mid].isIncart;
+    this.setState({
+      movies,
+    });
+  };
+
+  handleToggleFAv = (movie) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(movie);
+    movies[mid].fav = !movies[mid].fav;
+    this.setState({
+      movies,
+    });
+  };
+  render() {
+    return (
+      <>
+        <Navbar />
+        <MovieList
+          movies={this.state.movies}
+          handleIncStar={this.handleIncStar}
+          handleDecStar={this.handleDecStar}
+          handleToggleCart={this.handleToggleCart}
+          handleToggleFAv={this.handleToggleFAv}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,122 +1,25 @@
 import { Component } from "react";
 import MovieCard from "./Moviecard";
-
 class MovieList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: [
-        {
-          title: "The Avengers!",
-          plot: "Supernatural powers show in the movie.",
-          price: 199,
-          rating: 8.9,
-          stars: 0,
-          fav: false,
-          isIncart: false,
-        },
-        {
-          title: "Inception",
-          plot: "A thief who steals corporate secrets through dream-sharing technology.",
-          price: 249,
-          rating: 9.0,
-          stars: 0,
-          fav: false,
-          isIncart: false,
-        },
-        {
-          title: "The Dark Knight",
-          plot: "Batman faces the Joker in a battle for Gotham’s soul.",
-          price: 299,
-          rating: 9.1,
-          stars: 0,
-          fav: false,
-          isIncart: false,
-        },
-        {
-          title: "Interstellar",
-          plot: "A team of explorers travel through a wormhole to ensure humanity’s survival.",
-          price: 279,
-          rating: 8.7,
-          stars: 0,
-          fav: false,
-          isIncart: false,
-        },
-        {
-          title: "Iron Man",
-          plot: "A genius billionaire builds a high-tech suit to fight evil.",
-          price: 199,
-          rating: 8.5,
-          stars: 0,
-          fav: false,
-          isIncart: false,
-        },
-        {
-          title: "Spider-Man: No Way Home",
-          plot: "Spider-Man seeks help from Doctor Strange after his identity is revealed.",
-          price: 259,
-          rating: 8.4,
-          stars: 0,
-          fav: false,
-          isIncart: false,
-        },
-      ],
-    };
-  }
-
-  handleIncStar = (movie) => {
-    const { movies } = this.state;
-    const mid = movies.indexOf(movie);
-    if (movies[mid].stars >= 5) {
-      return;
-    }
-    movies[mid].stars += 0.5;
-    this.setState({
-      movies,
-    });
-  };
-
-  handleDecStar = (movie) => {
-    const { movies } = this.state;
-    const mid = movies.indexOf(movie);
-    if (movies[mid].stars <= 0) {
-      return;
-    }
-    movies[mid].stars -= 0.5;
-    this.setState({
-      movies,
-    });
-  };
-
-  handleToggleCart = (movie) => {
-    const { movies } = this.state;
-    const mid = movies.indexOf(movie);
-    movies[mid].isIncart = !movies[mid].isIncart;
-    this.setState({
-      movies,
-    });
-  };
-
-  handleToggleFAv = (movie) => {
-    const { movies } = this.state;
-    const mid = movies.indexOf(movie);
-    movies[mid].fav = !movies[mid].fav;
-    this.setState({
-      movies,
-    });
-  };
   render() {
-    const { movies } = this.state;
+    const {
+      movies,
+      handleIncStar,
+      handleDecStar,
+      handleToggleFAv,
+      handleToggleCart,
+    } = this.props;
+
     return (
       <>
         {movies.map((movie, indx) => (
           <MovieCard
             movies={movie}
             key={indx}
-            addStars={this.handleIncStar}
-            decStars={this.handleDecStar}
-            toggleFav={this.handleToggleFAv}
-            toggleCart={this.handleToggleCart}
+            addStars={handleIncStar}
+            decStars={handleDecStar}
+            toggleFav={handleToggleFAv}
+            toggleCart={handleToggleCart}
           />
         ))}
       </>

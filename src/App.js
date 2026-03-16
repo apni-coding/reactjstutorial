13bc2a35c9b6fd7b1c4774fdd596f86d5258e38f
apni-coding <apnicoding72@gmail.com>
2026-03-16 16:58:55 +0530

    what is hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import React from "react"
-import ComponentA from "./ComponentA"
-import Timer from "./Timer/TimerOne"
-import ErrorBoundary from "./ErrorBoundary";
-import ComponentB from "./ComponentB";
+import Input from "./Components/InputWithClass";
 
-class App extends React.Component{
-  constructor(){
-    super();
-
-    this.state={
-      play: false
-    }
-  }
-
-  handleClick = ()=>{
-    this.setState((prevState) =>({play: !prevState.play}))
-  }
-  render(){
-    return(
-      <>
-      <ErrorBoundary>
-        <ComponentA />
-      </ErrorBoundary>
-
-       <ErrorBoundary>
-        <ComponentB />
-      </ErrorBoundary>
-      </>
-
-    )
-  }
+function App() {
+  return (
+    <>
+      <Input />
+    </>
+  );
 }
 
-export default App
+export default App;

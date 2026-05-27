0bd565c4c0368509ea6de2a1c834516be6be2b08
apni-coding <apnicoding72@gmail.com>
2026-05-27 16:20:10 +0530

    local storage in react js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import Blog from "./Components/Blog";
+import Login from "./Components/Login";
+import Reset from "./Components/Reset";
+
+import { useState } from "react";
 
 function App() {
+  const [form, setForm] = useState("login");
+
   return (
-    <>
-      <Blog />
-    </>
+    <div className="App">
+      <h1>Welcome!</h1>
+      {form === "login" ? <Login /> : <Reset />}
+      <button
+        onClick={() => {
+          setForm(form === "login" ? "reset" : "login");
+        }}
+      >
+        {form === "login" ? "Forgot Password" : "Back to Login"}
+      </button>
+    </div>
   );
 }
 

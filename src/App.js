c128735d1290878f8a508355b1162323f242ed2b
apni-coding <apnicoding72@gmail.com>
2026-06-04 14:47:50 +0530

    starter code for react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import Blog from "./Components/Blog";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Items from "./pages/Items";
 
 function App() {
+  const [page, setPage] = useState("home");
   return (
     <>
-      <Blog />
+      <Navbar setPage={setPage}/>
+      {page === "home" && <Home />}
+      {page === "about" && <About />}
+      {page === "items" && <Items />}
     </>
   );
 }

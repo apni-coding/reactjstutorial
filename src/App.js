fc3aafc25194c8f454a12cf19d5412f0440e1ae2
apni-coding <apnicoding72@gmail.com>
2026-06-11 15:04:02 +0530

    creating routes using react router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Items from "./pages/Items";
-
+import { Routes, Route } from "react-router";
 function App() {
   const [page, setPage] = useState("home");
   return (
     <>
-      <Navbar setPage={setPage}/>
+      {/* <Navbar setPage={setPage}/>
       {page === "home" && <Home />}
       {page === "about" && <About />}
-      {page === "items" && <Items />}
+      {page === "items" && <Items />} */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/items" element={<Items />} />
+      </Routes>
     </>
   );
 }

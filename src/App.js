fd189f62c0330e5254cea56028458d623f6506c2
apni-coding <apnicoding72@gmail.com>
2026-06-17 20:54:09 +0530

    unhandled routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Routes, Route } from "react-router";
 import Mobile from "./pages/Mobile";
 import Laptop from "./pages/Laptop";
 import ItemsDetails from "./pages/ItemsDetails";
+import ErrorPage from "./pages/ErrorPage";
 function App() {
   const [page, setPage] = useState("home");
   return (
@@ -21,9 +22,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/items" element={<Items />} />
         <Route path="/items/:id" element={<ItemsDetails />} />
-
-          
-        
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
       {/* Footer */}
     </>
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ErrorPage() {
+  return (
+    <div>
+      <h1>OOPS!! Something Went Wrong</h1>
+    </div>
+  )
+}

ac6fcc65fedd35869084d71714ee91d8017cb955
apni-coding <apnicoding72@gmail.com>
2026-06-24 16:24:40 +0530

    course detail page dynamic route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Hero from "./pages/hero/Hero";
 import Nav from "./components/nav/Nav";
 import Courses from "./pages/courses/Courses";
 import { Route, Routes } from "react-router";
+import Details from "./pages/details/Details";
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
       <Hero />
       <Courses /> */}
       <Routes>
-        <Route path="/" element={<Nav />} >
-            {/* <Route  */}
+        <Route path="/" element={<Nav />}>
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/courses/:id" element={<Details />} />
         </Route>
       </Routes>
     </div>
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,25 +1,33 @@
 import React from "react";
 import style from "./Nav.module.css";
-import { Outlet } from "react-router";
+import { Link, NavLink, Outlet } from "react-router";
 
 function Nav() {
   return (
-    <nav>
-      <div className={style.nav_container}>
-        <div className={style.nav_title_wrapper}>
-          <img
-            className={style.logo}
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcQx67r6g5bhYxznUcR-2cKqYGJxojnJCWt0dY3fH41g&s=10"
-            alt="logo"
-          />
-          <h4>Apni Coding</h4>
+    <>
+      <nav>
+        <div className={style.nav_container}>
+          <div className={style.nav_title_wrapper}>
+            <Link to="/">
+              <img
+                className={style.logo}
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcQx67r6g5bhYxznUcR-2cKqYGJxojnJCWt0dY3fH41g&s=10"
+                alt="logo"
+              />
+            </Link>
+            <h4>Apni Coding</h4>
+          </div>
+          <div className={style.nav_details}>
+            <button>
+              <NavLink to="/courses">
+                {({ isActive }) => (isActive ? "On Courses" : "Go To Courses")}
+              </NavLink>
+            </button>
+          </div>
         </div>
-        <div className={style.nav_details}>
-          <button>Courses</button>
-        </div>
-      </div>
+      </nav>
       <Outlet />
-    </nav>
+    </>
   );
 }
 

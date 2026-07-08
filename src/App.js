f2a85fd97436805c8fedec4daf5a923c31d792d3
apni-coding <apnicoding72@gmail.com>
2026-07-08 22:06:58 +0530

    shopping-cart-starter-code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import ParentComponent from "./components/ParentComponent";
+import "./App.css";
+import { useState } from "react";
+import Items from "./components/Items";
+import Navbar from "./components/Navbar";
 
-const App = () => {
-  return <ParentComponent />;
-};
-
+function App() {
+  const [total, setTotal] = useState(0);
+  const [item, setItem] = useState(0);
+  return (
+    <div className="App">
+      <h2>Shopping Cart</h2>
+      <Navbar />
+      <Items />
+    </div>
+  );
+}
 export default App;

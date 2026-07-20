ff93d686505bfbd0d45be7e3fe4aa4c22966426d
apni-coding <apnicoding72@gmail.com>
2026-07-20 12:13:55 +0530

    multiple context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React, { useContext } from "react";
 import styles from "../styles/Total.module.css";
 import { itemContext } from "../itemContext";
+import { totalContext } from "../totalContext";
 
 function Navbar() {
-  const value = useContext(itemContext);
+  const {item} = useContext(itemContext);
+  const {total} = useContext(totalContext)
 
   return (
     <div className={styles.container}>
-      <h1>Total : &#x20B9; {value.total}</h1>
-      <h1>Items: {value.item}</h1>
+      <h1>Total : &#x20B9; {total}</h1>
+      <h1>Items: {item}</h1>
     </div>
   );
 }

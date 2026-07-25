0d6cdd07e1e56012e5831c5aa57ef6936447b3c7
apni-coding <apnicoding72@gmail.com>
2026-07-25 11:41:54 +0530

    custom provider

--- src/itemContext.js
+++ src/itemContext.js
@@ -1,5 +1,16 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const itemContext = createContext();
 
-export {itemContext}
+function CustomItemContext({ children }) {
+  const [total, setTotal] = useState(0);
+  const [item, setItem] = useState(0);
+  return (
+    <itemContext.Provider value={{ total, setTotal, item, setItem }}>
+      {children}
+    </itemContext.Provider>
+  );
+}
+
+export { itemContext };
+export default CustomItemContext;

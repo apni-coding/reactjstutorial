6f3f5b9c40ab74cb33ea885d78b98a34c5f17d30
apni-coding <apnicoding72@gmail.com>
2026-08-23 21:17:50 +0530

    make card dynamic

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,10 +1,18 @@
+import data from "../data/itemData";
 import styles from "../styles/Item.module.css";
 import ItemCard from "./ItemCard";
 
 function Items() {
   return (
     <div className={styles.wrapper}>
-      <ItemCard name="Apple" price={199} />
+      {data.map((item) => (
+        <ItemCard
+          name={item.name}
+          price={item.price}
+          key={item.id}
+          id={item.id}
+        />
+      ))}
     </div>
   );
 }

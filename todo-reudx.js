d62460c24fbb3f98739736a9f798ee16868f9e2c
apni-coding <apnicoding72@gmail.com>
2026-09-03 21:54:30 +0530

    complted reducer functions

--- todo-reudx.js
+++ todo-reudx.js
@@ -5,20 +5,39 @@ const ADD_TODO = "Add TODO";
 const TOGGLE_TODO = "Toggle TODO";
 
 // Action Creators
-const addTodo =(text)=>({type:ADD_TODO, text });
-const toggleToDo = (index)=> ({type: TOGGLE_TODO, index});
+const addTodo = (text) => ({ type: ADD_TODO, text });
+const toggleToDo = (index) => ({ type: TOGGLE_TODO, index });
 
 // Initial State
-const inititalState ={
-    todo:[]
+const inititalState = {
+    todo: [],
 }
 
 // Reducers
-function todoReducer(state=inititalState, action){
+function todoReducer(state = inititalState, action) {
 
-    switch(action.type){
+    switch (action.type) {
         case ADD_TODO:
+            return {
+                ...state,
+                todo: [
+                    ...state.todo,
+                    {
+                        text: action.text,
+                        complted: false
+                    }
+                ]
+            }
         case TOGGLE_TODO:
+            return {
+                ...state,
+                todos: state.todo.map((todo, i)=>{
+                    if(i === action.index){
+                        todo.complted = !todo.complted;
+                    }
+                    return todo;
+                })
+            }
         default:
             return state;
     }
